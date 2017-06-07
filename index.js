f5328b0fd61935ae68bdc4b2591734f2ed5d44a1
Ev0luti0n@89 <vmutshinya@parliament.gov.za>
2017-06-07 13:56:16 +0200

    Message timestamp and user left

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var participants = {};
+
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(req, res){
@@ -10,21 +12,31 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
-    io.sockets.emit('chat message', {username : msg.username, message : msg.message});
+    io.sockets.emit('chat message', {username : msg.username, message : msg.message, dateTime : msgTimeStamp()});
 	if(msg.message) console.log(msg.username + " said " + msg.message);
   });
-  socket.on('disconnect', function(msg){
-    console.log(msg.username + ' disconnected');
+  socket.on('disconnect', function(){
+	io.sockets.emit('left', {userLeft : participants[socket.id]});
+	delete participants[socket.id]};
+    console.log(participants[socket.id] + ' disconnected');
   });
 });
 
 io.on('connection', function(socket){
   socket.on('join', function(msg){
-	io.sockets.emit('join', {username : msg.username});
-	console.log(msg.username + " username");
+	io.sockets.emit('join', {username : msg.username, dateTime : msgTimeStamp()});
+	participants[socket.id] = msg.username;
+	console.log("socket ID user " + participants[socket.id]);
   });
 });
 
 http.listen(app.get('port'), function(){
   console.log('listening on *:' + app.get('port'));
 });
+
+function msgTimeStamp()
+{
+	var dateTime = new Date();
+	var timeStamp = "(" + dateTime.getHours() + ":" + dateTime.getMinutes() + ") ";
+	return timeStamp;
+}
