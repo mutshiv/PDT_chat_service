44146c45683e114df1c08b3d4e00b1c6b22d8771
Mutshiv <vmutshinya@parliament.gov.za>
2017-07-06 10:59:18 +0200

    Notice module deployment

--- index.js
+++ index.js
@@ -10,25 +10,38 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/app/chat.html');
 });
 
-io.on('connection', function(socket){
+app.get('/notices', function(req, res){
+  res.sendFile(__dirname + '/app/index.html');
+});
+
+io.on('connection', function(socket)
+{
   socket.on('chat message', function(msg){
     io.sockets.emit('chat message', {username : msg.username, message : msg.message, dateTime : msgTimeStamp()});
 	if(msg.message) console.log(msg.username + " said " + msg.message);
   });
+  
   socket.on('disconnect', function(){
 	if(Object.keys(participants).length > 0){
 		io.sockets.emit('left', {userLeft : participants[socket.id]});
 		console.log(participants[socket.id] + ' disconnected');
 		delete participants[socket.id];
 	}
+	console.log(socket.id + ' disconnected');
+  });
+  
+  socket.on('noticemsg', function(msg){
+	console.log('Event Title: ' + msg.eventTitle + ',\nNotice Type: ' + msg.noticeType + ',\nMessage: ' + msg.noticeMsg + ',\nLocation: ' + msg.eventlocation + ',\nEvent Date: ' + msg.eventDate + "\n");
+	io.sockets.emit('noticemsg', msg);
   });
 });
-
-io.on('connection', function(socket){
+  
+io.on('connection', function(socket)
+{
   socket.on('join', function(msg){
 	io.sockets.emit('join', {username : msg.username, dateTime : msgTimeStamp()});
 	participants[socket.id] = msg.username;
-	console.log("Socket ID user " + participants[socket.id]);
+	console.log("socket ID user " + participants[socket.id]);
   });
 });
 
